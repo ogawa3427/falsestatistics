9c6a0debf157b17ceca1aa3b035578b1534249a8
Ogawa34 <rst34shimizuku@stu.kanazawa-u.ac.jp>
2023-07-10 06:32:49 +0900

    inti_wo_sukosi

---
+++ aup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#get version number
+#ver=`grep -c '^' ~/ogawabox/releasenote.txt`
+
+#echo $ver
+#nver=$((ver + 1))
+#echo ver
+#echo $nver
+
+#get date
+#dat=`/bin/date '+%Y%m%d%H%M%S'`
+#echo $dat
+
+#get title or summary
+echo Title?
+read tit
+
+#get article
+#echo article?
+#read art
+
+#writedown
+#echo $nver $dat $tit $art >> ~/ogawabox/releasenote.txt
+
+#gitting
+git add -A
+#echo $nver $tit > temp_for_commit
+#comimess=$(cat temp_for_commit)
+#git commit -m "$comimess"
+#cmes=`echo $nver $tit`
+#eho $cmes
+git commit -m "$tit"
+git push origin souji
+#rm temp_for_commit
+
+exit
