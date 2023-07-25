33ef108da9e8e29ce271ed22454b2539308e6ae4
Ogawa34 <rst34shimizuku@stu.kanazawa-u.ac.jp>
2023-07-26 01:44:49 +0900

    mache

--- sbm/tester.sh
+++ sbm/tester.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-echo file
+echo "$ ./max file"
 ./max mitest
+echo ""
 
-echo pppipe
+echo "$ cat mitest | ./max"
 cat mitest | ./max
+echo ""
 
-echo redirect
+echo "$ ./max < mitest"
 ./max < mitest
+echo ""
+
+echo "$ ./max -h"
+./max -h
+echo ""
+
+echo "$ ./max --help"
+./max --help
+echo ""
+
+echo "$./max -e mitest"
+./max -e mitest
+echo ""
+
+echo "$./max --escape mitest"
+./max -e mitest
+echo ""
+
+echo "$ ./max 1 2 3 4 "
+./max 1 2 3 4
+echo ""
 exit
