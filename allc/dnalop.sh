87d8a23527d4db44c8762980b4f2a17539cc3416
ogawa3427 <rst34shimizuku@stu.kanazawa-u.ac.jp>
2023-08-05 19:20:54 +0900

    kirei

--- allc/dnalop.sh
+++ allc/dnalop.sh
@@ -1,122 +1,76 @@
 #!/bin/bash
 
 function gyaku () {
-	echo -n "" > temp
+	echo -n "" > stemp
 	exec 3<"$1"
-
+	while true; do
+        IFS= read -r -n1 -u3 char
+        if [ -z "$char" ]; then
+        	break
+        fi
+        echo -n "$char" > onetemp
+        cat onetemp stemp > temptemp
+        rm stemp
+        rm onetemp
+        mv temptemp stemp
+    done   
+    exec 3<&-
+    rm "$1"
+    mv stemp "$1"
 }
 
 function add () {
-
-#逆順にする
 	echo "add" >&2
-	exec 3<"tempf"
-    exec 4<"temps"
 
-    echo -n "" > ftemp
-    echo -n "" > stemp
-    echo -n "" > sgs
-    echo -n "" > sgf
-
-    while true; do
-        IFS= read -r -n1 -u3 char1
-        IFS= read -r -n1 -u4 char2
-
-        if [ -z "$char1" ] && [ -z "$char2" ]; then
-            break #終わったら抜ける
-        fi
-
-  #      [ -z "$char1" ] && char1="0"
-  #      [ -z "$char2" ] && char2="0" そなたはまだ力を開放すべきではない
-
-		#頭に来ますね(加賀並感)
-		echo -n $char1  > gf
-		cat gf sgf > ngf
-		rm  sgf
-		mv  ngf sgf
-
-		echo -n $char2  > gs
-		cat gs sgs > ngs
-		rm sgs
-		mv ngs sgs
-
-    done
-
-    exec 3<&-
-    exec 4<&-
+	gyaku farg
+	gyaku sarg
 
 #たしざん&0埋め
+	exec 3<"farg"
+	exec 4<"sarg"
 
-exec 3<"sgf"
-exec 4<"sgs"
-
-flag=0
-echo -n > rawout
-
-while true; do
-  IFS= read -r -n1 -u3 char1
-  IFS= read -r -n1 -u4 char2
-
-  if [ -z "$char1" ] && [ -z "$char2" ]; then
-    # 両方のファイルが終了した場合、ループを抜ける
-    break
-  fi
-
-  # もしchar1が空なら0を設定
-  [ -z "$char1" ] && char1="0"
-
-  # もしchar2が空なら0を設定
-  [ -z "$char2" ] && char2="0"
-
-#ワイの答えはこれや！
-  sum=$((char1 + char2))
-	sum=$((sum + flag))
-
-#記録&go next
-if [ $sum -lt 10 ]; then
-	echo -n $sum | sed 's/0/z/g' >> rawout
 	flag=0
-elif [ $sum -eq 10 ]; then
-	echo -n "z" >> rawout
-	flag=1
-else
-	sum=$((sum - 10))
-	echo -n $sum >> rawout
-	flag=1
-fi
+	echo -n > rawout
 
-done
+	while true; do
+  		IFS= read -r -n1 -u3 char1
+  		IFS= read -r -n1 -u4 char2
 
-# ファイルディスクリプタを閉じる
-exec 3<&-
-exec 4<&-
+  		if [ -z "$char1" ] && [ -z "$char2" ]; then
+    # 両方のファイルが終了した場合、ループを抜ける
+    		break
+  		fi
 
-echo $flag | sed 's/0//g' >> rawout
+  		# もしchar1が空なら0を設定
+  		[ -z "$char1" ] && char1="0"
+  		# もしchar2が空なら0を設定
+  		[ -z "$char2" ] && char2="0"
 
-#逆順にする
-	exec 3<"rawout"
+		#ワイの答えはこれや！
+  		sum=$((char1 + char2))
+		sum=$((sum + flag))
 
-    echo -n "" > otemp
-    echo -n "" > so
+		#記録&go next
+		if [ $sum -lt 10 ]; then
+			echo -n "$sum" | sed 's/0/z/g' >> rawout
+			flag=0
+		elif [ $sum -eq 10 ]; then
+			echo -n "z" >> rawout
+			flag=1
+		else
+			sum=$((sum - 10))
+			echo -n $sum >> rawout
+			flag=1
+		fi
+	done
+	exec 3<&-
+	exec 4<&-
 
-    while true; do
-        IFS= read -r -n1 -u3 char1
+	echo $flag | sed 's/0//g' >> rawout
+	gyaku rawout
 
-        if [ -z "$char1" ] ; then
-            break #終わったら抜ける
-        fi
 
-		#頭に来ますね(加賀並感)
-		echo -n $char1  > go
-		cat go so > no
-		rm  so
-		mv  no so
-
-    done
-
-    exec 3<&-
-
-cat so | sed 's/z/0/g'
+cat rawout | sed 's/z/0/g'
 echo
 
 rm g*
@@ -140,8 +94,8 @@ function div () {
 	echo div
 }
 
-echo "$1" > tempf
-echo "$2" > temps
+echo "$1" > farg
+echo "$2" > sarg
 
 if [ $# -eq 3 ]; then
 	case $3 in
@@ -165,6 +119,6 @@ else
 	echo "error! ./allc.sh (val) (val) (ope) "
 fi
 
-rm tempf
-rm temps
+rm farg
+rm sarg
 exit
